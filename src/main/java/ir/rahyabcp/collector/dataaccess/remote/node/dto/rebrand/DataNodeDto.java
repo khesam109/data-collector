aec8165fcfdf5a1d2d0a0175672939c84db5159d
khesam109 <khesam109@gmail.com>
2025-05-11 21:52:51 +0330

    Refactor DataNodeDto to include JsonTypeInfo and JsonSubTypes for extended properties
    Signed-off-by: khesam109 <khesam109@gmail.com>

--- src/main/java/ir/rahyabcp/collector/dataaccess/remote/node/dto/rebrand/DataNodeDto.java
+++ src/main/java/ir/rahyabcp/collector/dataaccess/remote/node/dto/rebrand/DataNodeDto.java
@@ -2,19 +2,49 @@ package ir.rahyabcp.collector.dataaccess.remote.node.dto.rebrand;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-import java.util.Map;
 
+/**
+ * <a href="https://stackoverflow.com/questions/18757431/jackson-jsontypeinfo-as-external-property-doesnt-work-as-expected">Jackson JsonTypeInfo</a>
+ *
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataNodeDto(
 
-        @JsonProperty("datanode_id") int dataNodeId,
-        @JsonProperty("datanode_name") String dataNodeName,
-        @JsonProperty("datanode_status") String dataNodeStatus,
-        @JsonProperty("dc_contentmode") String contentMode,
-        @JsonProperty("dc_protocol") String protocol,
-        @JsonProperty("description") String description,
-        @JsonProperty("extende_properties") Map<String, Object> extendedProperties,
-        @JsonProperty("dataformat") DataFormatDto dataFormat
+        @JsonProperty("datanode_id")
+        int dataNodeId,
+
+        @JsonProperty("datanode_name")
+        String dataNodeName,
+
+        @JsonProperty("datanode_status")
+        String dataNodeStatus,
+
+        @JsonProperty("dc_contentmode")
+        String contentMode,
+
+        @JsonProperty("dc_protocol")
+        String protocol,
+
+        @JsonProperty("description")
+        String description,
+
+        @JsonProperty("dataformat")
+        DataFormatDto dataFormat,
+
+        @JsonTypeInfo(
+                use = JsonTypeInfo.Id.NAME,
+                include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+                property = "dc_protocol",
+                visible = true
+        )
+        @JsonSubTypes({
+                @JsonSubTypes.Type(value = FtpPropertiesDto.class, name = "FTP"),
+                @JsonSubTypes.Type(value = RdbPropertiesDto.class, name = "RDB")
+        })
+        @JsonProperty("extende_properties")
+        ExtendedPropertiesDto extendedProperties
 ) {
 }
--- src/main/java/ir/rahyabcp/collector/dataaccess/remote/node/dto/rebrand/ExtendedPropertiesDto.java
+++ src/main/java/ir/rahyabcp/collector/dataaccess/remote/node/dto/rebrand/ExtendedPropertiesDto.java
@@ -1,20 +1,8 @@
 package ir.rahyabcp.collector.dataaccess.remote.node.dto.rebrand;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "dc_protocol",
-        visible = true
-)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = FtpPropertiesDto.class, name = "FTP"),
-        @JsonSubTypes.Type(value = RdbPropertiesDto.class, name = "RDB")
-})
-//@JsonDeserialize(using = ExtendedPropertiesDeserializer.class)
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class ExtendedPropertiesDto {
 }
